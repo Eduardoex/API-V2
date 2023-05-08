2260d631f6258922edcd132e1ae5caa75caaf7b1
Eduardo de Lima <eduardodelimaex@gmail.com>
2023-05-08 01:41:47 -0300

    crypto password

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,15 +1,26 @@
+const {hash} = require("bcryptjs")
 const AppError = require("../utils/AppError")
+const sqliteConnection = require("../database/sqlite")
 
 class UserController {
-  create(request, response){
+  async create(request, response){
     const {name, email, password} = request.body
 
-    if(!name){
-      throw new AppError("Nome é obrigatorio")
+   const database = await sqliteConnection();
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?) ", [email])
+
+    if(checkUserExists){
+      throw new AppError("esse e-mail já está em uso")
     }
 
+const hashedPassword = await hash(password, 8)
 
-    response.status(201).json({name, email, password})
+
+    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+    [name, email, hashedPassword]) 
+
+
+    return response.status(201).json()
   }
 }
 
